18d69250334623cf9ceae4ae9f04e9c77123723d
SariSafe <98646866+SariSafe@users.noreply.github.com>
2022-03-17 23:24:31 +0200

    Update file3.sql
    get advanced in 4 ques..............

--- file3.sql
+++ file3.sql
@@ -9,6 +9,23 @@
 
 
 
+ #נסיון לפתור שאלה  4
+
+
+ select distinct  importantDate.OrderDate,FirstName,employees.EmployeeID,LastName,employees.HomePhone
+ from orders,employees,
+ (select orders.EmployeeID,orders.OrderDate
+  from
+  orders where orders.EmployeeID =8 
+  and OrderDate<='1998-05-08 00:00:00'
+  and  OrderDate >='1998-05-01 00:00:00')
+   as importantDate 
+  where employees.EmployeeID <> 8
+
+
+
+
+
 
 
 
