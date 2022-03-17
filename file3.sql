74e1387ddea3c49bcd6b5606b84477af8acf71f4
SariSafe <98646866+SariSafe@users.noreply.github.com>
2022-03-17 23:26:18 +0200

    Update file3.sql
    that not finished should be like note...............

--- file3.sql
+++ file3.sql
@@ -12,15 +12,15 @@
  #נסיון לפתור שאלה  4
 
 
- select distinct  importantDate.OrderDate,FirstName,employees.EmployeeID,LastName,employees.HomePhone
- from orders,employees,
- (select orders.EmployeeID,orders.OrderDate
-  from
-  orders where orders.EmployeeID =8 
-  and OrderDate<='1998-05-08 00:00:00'
-  and  OrderDate >='1998-05-01 00:00:00')
-   as importantDate 
-  where employees.EmployeeID <> 8
+#select distinct  importantDate.OrderDate,FirstName,employees.EmployeeID,LastName,employees.HomePhone
+#from orders,employees,
+#(select orders.EmployeeID,orders.OrderDate
+#from
+#orders where orders.EmployeeID =8 
+#and OrderDate<='1998-05-08 00:00:00'
+#and  OrderDate >='1998-05-01 00:00:00')
+#as importantDate 
+#where employees.EmployeeID <> 8
 
 
 
