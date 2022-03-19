c7fd382f0ac573a74d0b4acd070ff50bfc2a04fd
SariSafe <98646866+SariSafe@users.noreply.github.com>
2022-03-19 20:44:00 +0200

    Update file3.sql
    solution of 3 is done

--- file3.sql
+++ file3.sql
@@ -1,10 +1,10 @@
 
-#שאלה 3 במסדי נתונים 
-#select distinct ProductID,ProductName,SupplierID,CompanyName,Phone,100-(UnitsOnOrder+UnitsInStock)as orderto
-#from 
-#(select products.ProductID,products.ProductName,products.UnitsOnOrder,products.UnitsInStock,products.SupplierID,CompanyName,Phone from products,suppliers
-# where UnitsOnOrder+UnitsInStock<10
-#  and  suppliers.SupplierID=products.SupplierID )as main
+#שאלה 3 במסדי נתונים פתרון   
+select distinct ProductID,ProductName,SupplierID,CompanyName,Phone,100-(UnitsOnOrder+UnitsInStock)as orderto
+from 
+(select products.ProductID,products.ProductName,products.UnitsOnOrder,products.UnitsInStock,products.SupplierID,CompanyName,Phone from products,suppliers
+where UnitsOnOrder+UnitsInStock<10
+ and  suppliers.SupplierID=products.SupplierID )as main
 
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
