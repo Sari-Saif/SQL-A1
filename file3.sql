8fad0e7c8e29835981f24cb4df1facfa2538e76c
SariSafe <98646866+SariSafe@users.noreply.github.com>
2022-03-22 13:18:13 +0200

    Update file3.sql
    ques 3+4 done .

--- file3.sql
+++ file3.sql
@@ -1,5 +1,5 @@
 
-#שאלה 3 במסדי נתונים פתרון   
+#שאלה 3  פתרון  
 select distinct ProductID,ProductName,SupplierID,CompanyName,Phone,100-(UnitsOnOrder+UnitsInStock)as orderto
 from 
 (select products.ProductID,products.ProductName,products.UnitsOnOrder,products.UnitsInStock,products.SupplierID,CompanyName,Phone from products,suppliers
@@ -10,34 +10,10 @@ where UnitsOnOrder+UnitsInStock<10
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
- #נסיון לפתור שאלה  4
+ # פתרון  שאלה  4
 
 
-#select distinct  importantDate.OrderDate,FirstName,employees.EmployeeID,LastName,employees.HomePhone
-#from orders,employees,
-#(select orders.EmployeeID,orders.OrderDate
-#from
-#orders where orders.EmployeeID =8 
-#and OrderDate<='1998-05-08 00:00:00'
-#and  OrderDate >='1998-05-01 00:00:00')
-#as importantDate 
-#where employees.EmployeeID <> 8
-
-כאן אני מוצא את כל העובדים שעבדו  בשבוע האחרון  כך  שעובד בתז שלו  שווה 8  גם  עבד איתם אני 
-עכשיו מנסה להצליב עוד נתונים שאוכל להתקדם  האלה אם  אני מוציא את תאריך האחרון
-שעבדו  יחד אני מסיים  את חיפוש שלי ואז אוכל לבנות את שאלתה הרצויה כנל לגבי הטבלה 
-
- #select
-  #distinct orders.OrderDate,employees.EmployeeID
-  #from orders,employees
-  #where OrderDate <'1998-05-08 00:00:00'
-  #and  OrderDate >'1998-05-01 00:00:00' 
-  #and employees.EmployeeID=orders.EmployeeID 
-  #and employees.EmployeeID <> 8
-
-
-שאלה 4 נפתרה גם 
-select  employees.EmployeeID,employees.FirstName,employees.LastName,employees.HomePhone
+select  employees.EmployeeID,employees.FirstName,employees.LastName,employees.HomePhone,DATE_ADD('1998-05-06 00:00:00', INTERVAL 7 DAY ) as 'insulation ends at...'
  from employees,orders
  where employees.EmployeeID  <> 8 and employees.EmployeeID= orders.EmployeeID  and orders.OrderDate in 
 (SELECT distinct max(orders.OrderDate) FROM
