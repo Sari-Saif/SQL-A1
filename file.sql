1e367c88c7679508a033b12dd06e47f3354463b4
SariSafe <98646866+SariSafe@users.noreply.github.com>
2022-03-26 19:25:08 +0300

    it's done baby !

--- file.sql
+++ file.sql
@@ -1,4 +1,18 @@
 
+
+#שאלה 2  פתרון
+
+select products.ProductID , 
+products.ProductName ,
+ sum(`order details` .Quantity) as 'totalQuantity' ,
+concat('$' , cast(round(sum(`order details` .UnitPrice*`order details` .Quantity),2) as char)) as 'totalPrice',
+concat('$' , cast(round(sum(`order details` .UnitPrice*`order details`.Quantity)/(sum(`order details`.Quantity)),2) as char)) as 'wightAvgPrice',
+concat('$' , cast(round(avg(`order details` .UnitPrice), 2) as char)) as AvgPrice,
+concat('$' , cast(round(max(`order details` .UnitPrice), 2) as char)) as currentPrice
+from products,`order details` 
+ where products.ProductID = `order details`.ProductID group by products.ProductID
+ order by max(`order details` .UnitPrice)
+
 #שאלה 3  פתרון  
 select distinct ProductID,ProductName,SupplierID,CompanyName,Phone,100-(UnitsOnOrder+UnitsInStock)as orderto
 from 
@@ -6,9 +20,6 @@ from
 where UnitsOnOrder+UnitsInStock<10
  and  suppliers.SupplierID=products.SupplierID )as main
 
-
--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
  #  שאלה  4 פתרון   
 
@@ -21,9 +32,6 @@ select  employees.EmployeeID,employees.FirstName,employees.LastName,employees.Ho
  where orders.EmployeeID = 8 and orders.OrderDate 
  between  '1998-05-01 00:00:00' and '1998-05-08 00:00:00')
  group by employees.LastName
-
------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 #  שאלה  5 פתרון
